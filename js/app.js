ea18a7f45c2dcb35d16f1c3cac1ee087c766fef7
AngerDESTROYS <Nehrim24@gmail.com>
2024-01-28 14:25:37 +0200

    smooth commit

--- js/app.js
+++ js/app.js
@@ -17,6 +17,7 @@ function update(cursorPosition) {
     let zValue =
       (cursorPosition - parseFloat(getComputedStyle(el).left)) * isInLeft * 0.1;
 
+    el.style.transition = 'transform 0.3s ease-out';  // Adjust the duration and easing as needed
     el.style.transform = `rotateY(${
       rotateDegree * rotateSpeed
     }deg) translateX(calc(-50% + ${
@@ -27,7 +28,7 @@ function update(cursorPosition) {
   });
 }
 
-update(500);
+update(0);
 
 window.addEventListener("mousemove", (e) => {
   if(timeline.isActive()) return;
